58dd892f7e27c63a039c5f6327aba9a0ed0e3b12
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-10-08 14:24:49 -0300

    Updated Player.java and Hand.java
    Updated Player and Hand to allow for splitting of a specific hand

--- src/game/Player.java
+++ src/game/Player.java
@@ -1,17 +1,34 @@
 package game;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Player {
 
-    private Hand mainHand;
-    private Hand splitHand;
+    private final List<Hand> handList;
 
     public Player() {
-        this.mainHand = null;
-        this.splitHand = null;
+        this.handList = new ArrayList<>();
     }
 
-    public Player (Hand initialHand) {
-        this.mainHand = initialHand;
-        this.splitHand = null;
+    public Player(Hand initialHand) {
+        this.handList = new ArrayList<>();
+        this.handList.add(initialHand);
+    }
+
+    public boolean splitHand(int index) {
+        // Restricting splits to a maximum of 4
+        if (handList.size() >= 4) {
+            return false;
+        }
+        Hand hand = handList.get(index);
+        if (hand.size() != 2 || hand.get(0).compareTo(hand.get(1)) != 0) {
+            return false;
+        }
+
+        handList.add(new Hand(hand.get(0)));
+        handList.add(new Hand(hand.get(1)));
+        handList.remove(index);
+        return true;
     }
 }
