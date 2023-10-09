efee77950f89454199c26083875724fef72d201c
Ethan Palser <39565969+ethpalser@users.noreply.github.com>
2023-10-09 20:55:19 -0300

    Updated Game.java and Settings
    Reduced complexity of main in Game and made Settings more flexible

--- src/blackjack/GameMode.java
+++ src/blackjack/GameMode.java
@@ -1,6 +1,16 @@
 package blackjack;
 
 public enum GameMode {
-    ALL_PLAYERS_VISIBLE,
-    NO_PLAYERS_VISIBLE,
+    ALL_PLAYERS_VISIBLE("All player hands are visible"),
+    NO_PLAYERS_VISIBLE("No player hands are visible");
+
+    private String displayValue;
+
+    GameMode(String displayValue){
+        this.displayValue = displayValue;
+    }
+
+    public String getDisplay() {
+        return this.displayValue;
+    }
 }
